8322878427cfba8da6ecba4756d4f0fec6c3c8d6
Alyssa Bull <alyssabull@gmail.com>
2020-10-24 15:58:00 -0600

    Add getUserSleepData to userRepository

---
+++ src/SleepRepository.js
@@ -0,0 +1,25 @@
+import Sleep from './Sleep';
+
+export default class SleepRepository {
+  constructor(sleepData) {
+    this.sleepHistory = [];
+
+  }
+
+  createSleep(id) {
+
+  }
+}
+
+
+
+
+
+// export default SleepRepository {
+//   constructor() {
+//     this.sleepHistory = []; // includes all instantiations of Sleep
+//     this.sleepQualityAverage = 0;
+//     this.sleepHoursRecord = [];
+//     this.sleepQualityRecord = [];
+//   }
+// }
