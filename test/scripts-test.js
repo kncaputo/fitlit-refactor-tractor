104af89b298b6bf0af42c262cca37389315228c1
Alyssa Bull <alyssabull@gmail.com>
2020-11-01 11:10:15 -0700

    Add fetch data test to chai spies

--- test/scripts-test.js
+++ test/scripts-test.js
@@ -1,6 +1,27 @@
-import '../src/scripts.js';
-import { expect } from 'chai'
-const spies = require('chai-spies'); 
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require('chai-spies');
 chai.use(spies);
+import Service from '../src/service/Service';
 
-describe('')
+describe.only('Service', () => {
+  before(() => {
+    global.service = {};
+    chai.spy.on(Service, ['postData', 'fetchData'], () => {})
+  })
+
+  describe('Fetch', () => {
+    it('should fetch user data from APIs', () => {
+      let service = new Service(url, dataKey)
+      expect(service.fetchData()).to.have.been.called(1);
+      expect(service.fetchData).to.have.been.called.with();
+    })
+  })
+
+  describe('Post', () => {
+    it('should post user data from input', () => {
+      expect(service.fetchData()).to.have.been.called(1);
+      expect(service.fetchData).to.have.been.called.with();
+    })
+  })
+})
