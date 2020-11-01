e554e960429f7e2ba9666fb4efdc7a352ee315ce
Bailey Dunning <bwdunning@gmail.com>
2020-11-01 09:42:05 -0700

    Clean up sleep test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 
 import Sleep from '../src/model/Sleep';
 
-describe('Sleep', () => {
-  let sleep;
-  let user1;
-  let user2;
-  let userRepository;
+describe.only('Sleep', () => {
+  let sleep1;
+  let sleep2;
+  let sleep3;
+
   beforeEach(() => {
     sleep1 = new Sleep({
       "userID": 1,
@@ -21,43 +21,38 @@ describe('Sleep', () => {
       "hoursSlept": 7.3,
       "sleepQuality": 3.2
     });
+
     sleep3 = new Sleep({
       "userID": 1,
       "date": "2019/07/17",
       "hoursSlept": 9.3,
       "sleepQuality": 1.4
-    }, userRepository);
-  });
-  it('should be a function', () => {
-    expect(Sleep).to.be.a('function');
-  });
-  it('should be an instance of activity', () => {
-    expect(sleep1).to.be.an.instanceof(Sleep);
-  });
-  it('should hold a userId', () => {
-    expect(sleep2.userId).to.equal(2);
-  });
-  it('should hold a date', () => {
-    expect(sleep3.date).to.equal("2019/07/17");
-  });
-  it('should hold hours slept', () => {
-    expect(sleep1.hoursSlept).to.equal(6.1);
-  });
-  it('should hold sleep quality', () => {
-    expect(sleep3.sleepQuality).to.equal(1.4);
-  });
-  describe('sleep', () => {
-    it('should update user\'s slept hours record', () => {
-      expect(user1.sleepHoursRecord.length).to.equal(2);
     });
-    it('should update user\'s slept hours record', () => {
-      expect(user2.sleepQualityRecord.length).to.equal(1);
+  });
+
+  describe('Constructor', () => {
+    it('should be a function', () => {
+      expect(Sleep).to.be.a('function');
     });
-    it('should update user\'s slept hours average', () => {
-      expect(user1.hoursSleptAverage).to.equal('7.7');
+
+    it('should be an instance of sleep', () => {
+      expect(sleep1).to.be.an.instanceof(Sleep);
     });
-    it('should update user\'s sleep quality average', () => {
-      expect(user1.sleepQualityAverage).to.equal('1.8');
+
+    it('should hold a userId', () => {
+      expect(sleep2.userID).to.equal(2);
     });
-  })
+
+    it('should hold a date', () => {
+      expect(sleep3.date).to.equal("2019/07/17");
+    });
+
+    it('should hold hours slept', () => {
+      expect(sleep1.hoursSlept).to.equal(6.1);
+    });
+
+    it('should hold sleep quality', () => {
+      expect(sleep3.sleepQuality).to.equal(1.4);
+    });
+  });
 });
